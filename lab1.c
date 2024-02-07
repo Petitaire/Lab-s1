9ca525efb567cadb933be468e817e566728b6d71
Kingee <158284164+Petitaire@users.noreply.github.com>
2024-02-07 16:28:10 +0100

    Create lab1.c

---
+++ lab1.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main(){
+  //task 1: print my name
+    printf("My name is QZ.\n");
+
+  //task 2: print a character 
+    char c;
+    printf("Enter a character: ");
+    scanf("%c", &c);
+    printf("%c %d\n",c, c); 
+  
+  //task 3: calculate sum of two integers
+    int a, b, sum;
+    float quotient;
+    printf("Enter two integer: ");
+    scanf("%d %d", &a, &b);
+    sum = a + b;
+    printf("The sum of %d and %d is %d\n", a, b, sum);
+  
+  //task4: calculate the quotient of two float numbers, print the numbers as float and integer
+    float num1, num2, result;
+    printf("Enter two numbers: ");
+    scanf("%f %f", &num1, &num2);
+    printf("The numbers you entered are [floats]: %f %f\n", num1, num2);
+    printf("The numbers you entered are [integers]: %d %d\n", (int)num1, (int)num2);
+    result = num1 / num2;
+    printf("%f divides %f is %f\n", num1, num2, result);
+
+return 0;
+}
